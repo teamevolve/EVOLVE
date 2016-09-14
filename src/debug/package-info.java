a3f29874d409f7b471ae32060b362066eb5957d7
Eric Collins <ericscollins@users.noreply.github.com>
2016-09-14 16:44:42 -0400

    Corrected author name

--- src/debug/package-info.java
+++ src/debug/package-info.java
@@ -2,7 +2,7 @@
  * This package contains all code related to the planned debugging framework 
  * for developers looking to extend the functionality of EVOLVE.
  *
- * @author eric
+ * @author ericscollins
  *
  */
 package debug;
