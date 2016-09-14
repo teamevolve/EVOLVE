ff80582ac5d0fa54be3fcc327519047ba43da432
Eric Collins <ericscollins@users.noreply.github.com>
2016-09-14 16:45:43 -0400

    Corrected author name

--- src/gui/package-info.java
+++ src/gui/package-info.java
@@ -2,7 +2,7 @@
  * This package contains all code related to the graphical front end of EVOLVE,
  * largely written using the Swing API.
  *
- * @author eric
+ * @author ericscollins
  *
  */
 package gui;
