b79f1c852f1d39faf8208cc027a271f1915242ac
Rich <richboy77777@gmail.com>
2016-09-29 02:02:31 -0400

    Added Population Capacity and Crash Capacity - needed in survl and gui

--- src/shared/SessionParameters.java
+++ src/shared/SessionParameters.java
@@ -27,12 +27,13 @@ import java.util.HashMap;
  * @author linneasahlberg
  * @author jasonfortunato
  * @author ericscollins
+ * @author richwenner
  *
  */
 public class SessionParameters {
 	// all members should private
 	
-	private int popSize, seed, numPops;
+	private int popSize, seed, numPops, popCapacity, crashCapacity;
 	private boolean popConst, fixedMig;
 	private HashMap<Genotype, Double> reproductionRates;
 	private HashMap<Genotype, Double> survivalRates;
@@ -145,5 +146,21 @@ public class SessionParameters {
 			sexualSelectionRates.put(gt1, new HashMap<Genotype, Double>());
 		}
 		sexualSelectionRates.get(gt1).put(gt2, rate);
+	}
+
+	public int getPopCapacity() {
+		return popCapacity;
+	}
+
+	public void setPopCapacity(int popCapacity) {
+		this.popCapacity = popCapacity;
+	}
+
+	public int getCrashCapacity() {
+		return crashCapacity;
+	}
+
+	public void setCrashCapacity(int crashCapacity) {
+		this.crashCapacity = crashCapacity;
 	}
 }
