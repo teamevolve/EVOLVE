9f958134f8e259bc837be023bb67b854c15785f4
adennis96 <adennis96@users.noreply.github.com>
2017-09-26 10:44:21 -0400

    Fixing three allele sexual selection gui

--- src/gui/SexSelectPane.java
+++ src/gui/SexSelectPane.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 import shared.Genotype;
 
 /**
- * 
+ *
  * @author linneasahlberg
  * @author jasonfortunato
  *
@@ -25,12 +25,12 @@ public class SexSelectPane extends EvoPane {
 
 	static final int TEXT_LEN_LONG = 8;
 	final static int TEXT_LEN_SHORT = 3;
-	
-	JLabel mateFreqLabel, AAPrefLabel,ABPrefLabel, BBPrefLabel, 
+
+	JLabel mateFreqLabel, AAPrefLabel,ABPrefLabel, BBPrefLabel,
 		ACPrefLabel, BCPrefLabel, CCPrefLabel;
 
 	JLabel AALabel, ABLabel, BBLabel, ACLabel, BCLabel, CCLabel;
-	
+
 	JTextField freqAAxAA, freqAAxAB, freqAAxBB, freqAAxAC, freqAAxBC, freqAAxCC;
 	JTextField freqABxAA, freqABxAB, freqABxBB, freqABxAC, freqABxBC, freqABxCC;
 	JTextField freqBBxAA, freqBBxAB, freqBBxBB, freqBBxAC, freqBBxBC, freqBBxCC;
@@ -38,12 +38,12 @@ public class SexSelectPane extends EvoPane {
 	JTextField freqBCxAA, freqBCxAB, freqBCxBB, freqBCxAC, freqBCxBC, freqBCxCC;
 	JTextField freqCCxAA, freqCCxAB, freqCCxBB, freqCCxAC, freqCCxBC, freqCCxCC;
 
-	ArrayList<Component> prefLabels = new ArrayList<Component>();	
+	ArrayList<Component> prefLabels = new ArrayList<Component>();
 	ArrayList<Component> labels = new ArrayList<Component>();
 	ArrayList<Component> fields = new ArrayList<Component>();
 
 	JPanel table;
-	
+
 	public SexSelectPane() {
 		super();
 		mateFreqLabel = new JLabel("<html><span style='font-size:11px'><b>Sexual Selection </b>(Mating Preference, 0.0-1.0): ");
@@ -60,14 +60,14 @@ public class SexSelectPane extends EvoPane {
 		ACLabel = new JLabel("AC"); threeAllelesList.add(ACLabel);
 		BCLabel = new JLabel("BC"); threeAllelesList.add(BCLabel);
 		CCLabel = new JLabel("CC"); threeAllelesList.add(CCLabel);
-		
+
 		freqAAxAA = new JTextField(TEXT_LEN_SHORT);
 		freqAAxAB = new JTextField(TEXT_LEN_SHORT);
 		freqAAxBB = new JTextField(TEXT_LEN_SHORT);
 		freqAAxAC = new JTextField(TEXT_LEN_SHORT); threeAllelesList.add(freqAAxAC);
 		freqAAxBC = new JTextField(TEXT_LEN_SHORT); threeAllelesList.add(freqAAxBC);
 		freqAAxCC = new JTextField(TEXT_LEN_SHORT); threeAllelesList.add(freqAAxCC);
-				
+
 		freqABxAA = new JTextField(TEXT_LEN_SHORT);
 		freqABxAB = new JTextField(TEXT_LEN_SHORT);
 		freqABxBB = new JTextField(TEXT_LEN_SHORT);
@@ -104,27 +104,27 @@ public class SexSelectPane extends EvoPane {
 		freqCCxCC = new JTextField(TEXT_LEN_SHORT); threeAllelesList.add(freqCCxCC);
 
 		addToLists();
-		
+
 		table = new JPanel();
 		table.setLayout(new GridBagLayout());
 		GridBagConstraints t = new GridBagConstraints(); // t for temp constraints
 		t.insets = new Insets(0, 0, 3, 15);
 
-		int k = 1; 
+		int k = 1;
 		t.gridx = 0;
 		t.gridy = 1;
 		for(Component comp : prefLabels) {
 			 t.gridy = k; k++;
 			 table.add(comp, t);
 		}
-				
-		int i = 1; 
+
+		int i = 1;
 		t.gridy = 0;
 		for(Component comp : labels) {
 			t.gridx = i; i++;
 			table.add(comp, t);
 		}
-		
+
 		i = 1; int j = 1;
 		for(Component comp : fields) {
 			t.gridx = i; i++;
@@ -132,16 +132,16 @@ public class SexSelectPane extends EvoPane {
 			table.add(comp, t);
 			if(i % 7 == 0 ) {
 				j++;
-				i = 1; 
+				i = 1;
 			}
 		}
-		
-		
+
+
 		c.gridx = 0; c.gridy = 1;
 		c.anchor = GridBagConstraints.WEST;
 		c.gridwidth = 3;
 		add(mateFreqLabel, c);
-			
+
 		//AA x __
 		c.gridx = 1; c.gridy = 2;
 		c.anchor = GridBagConstraints.WEST;
@@ -150,40 +150,40 @@ public class SexSelectPane extends EvoPane {
 		add(table, c);
 
 	}
-	
+
 	// does exactly what you'd think
 	private void addToLists() {
-		
+
 		prefLabels.add(AAPrefLabel);
 		prefLabels.add(ABPrefLabel);
 		prefLabels.add(BBPrefLabel);
 		prefLabels.add(ACPrefLabel);
 		prefLabels.add(BCPrefLabel);
 		prefLabels.add(CCPrefLabel);
-		
-		labels.add(AALabel);	labels.add(ABLabel);	labels.add(BBLabel);	
+
+		labels.add(AALabel);	labels.add(ABLabel);	labels.add(BBLabel);
 		labels.add(ACLabel);	labels.add(BCLabel);	labels.add(CCLabel);
-		
+
 		fields.add(freqAAxAA); fields.add(freqAAxAB); fields.add(freqAAxBB);
 		fields.add(freqAAxAC); fields.add(freqAAxBC); fields.add(freqAAxCC);
 
 		fields.add(freqABxAA); fields.add(freqABxAB); fields.add(freqABxBB);
 		fields.add(freqABxAC); fields.add(freqABxBC); fields.add(freqABxCC);
-		
+
 		fields.add(freqBBxAA); fields.add(freqBBxAB); fields.add(freqBBxBB);
 		fields.add(freqBBxAC); fields.add(freqBBxBC); fields.add(freqBBxCC);
 
 		fields.add(freqACxAA); fields.add(freqACxAB); fields.add(freqACxBB);
 		fields.add(freqACxAC); fields.add(freqACxBC); fields.add(freqACxCC);
-		
+
 		fields.add(freqBCxAA); fields.add(freqBCxAB); fields.add(freqBCxBB);
 		fields.add(freqBCxAC); fields.add(freqBCxBC); fields.add(freqBCxCC);
-		
+
 		fields.add(freqCCxAA); fields.add(freqCCxAB); fields.add(freqCCxBB);
 		fields.add(freqCCxAC); fields.add(freqCCxBC); fields.add(freqCCxCC);
-		
+
 	}
-	
+
 	@Override
 	public void setEnabled(boolean enable) {
 		super.setEnabled(enable);
@@ -191,38 +191,106 @@ public class SexSelectPane extends EvoPane {
 			c.setEnabled(enable);
 		}
 	}
-	
+
 	public void submit(shared.SessionParameters p) {
 		// AA x __
-		p.setSexualSelectionRate(Genotype.AA, Genotype.AA, 
+		p.setSexualSelectionRate(Genotype.AA, Genotype.AA,
 				Double.parseDouble(freqAAxAA.getText()));
-		p.setSexualSelectionRate(Genotype.AA, Genotype.AB, 
+		p.setSexualSelectionRate(Genotype.AA, Genotype.AB,
 				Double.parseDouble(freqAAxAB.getText()));
-		p.setSexualSelectionRate(Genotype.AA, Genotype.BB, 
+		p.setSexualSelectionRate(Genotype.AA, Genotype.BB,
 				Double.parseDouble(freqAAxBB.getText()));
-		
+
 		// AB x __
-		p.setSexualSelectionRate(Genotype.AB, Genotype.AA, 
+		p.setSexualSelectionRate(Genotype.AB, Genotype.AA,
 				Double.parseDouble(freqABxAA.getText()));
-		p.setSexualSelectionRate(Genotype.AB, Genotype.AB, 
+		p.setSexualSelectionRate(Genotype.AB, Genotype.AB,
 				Double.parseDouble(freqABxAB.getText()));
-		p.setSexualSelectionRate(Genotype.AB, Genotype.BB, 
+		p.setSexualSelectionRate(Genotype.AB, Genotype.BB,
 				Double.parseDouble(freqABxBB.getText()));
-		
+
 		// BB x __
-		p.setSexualSelectionRate(Genotype.BB, Genotype.AA, 
+		p.setSexualSelectionRate(Genotype.BB, Genotype.AA,
 				Double.parseDouble(freqBBxAA.getText()));
-		p.setSexualSelectionRate(Genotype.BB, Genotype.AB, 
+		p.setSexualSelectionRate(Genotype.BB, Genotype.AB,
 				Double.parseDouble(freqBBxAB.getText()));
-		p.setSexualSelectionRate(Genotype.BB, Genotype.BB, 
+		p.setSexualSelectionRate(Genotype.BB, Genotype.BB,
 				Double.parseDouble(freqBBxBB.getText()));
+
+    if(threeAlleles) {
+      // AA x C_
+      p.setSexualSelectionRate(Genotype.AA, Genotype.AC,
+  				Double.parseDouble(freqAAxAC.getText()));
+  		p.setSexualSelectionRate(Genotype.AA, Genotype.BC,
+  				Double.parseDouble(freqAAxBC.getText()));
+  		p.setSexualSelectionRate(Genotype.AA, Genotype.CC,
+  				Double.parseDouble(freqAAxCC.getText()));
+
+      // AB x C_
+      p.setSexualSelectionRate(Genotype.AB, Genotype.AC,
+  				Double.parseDouble(freqABxAC.getText()));
+  		p.setSexualSelectionRate(Genotype.AB, Genotype.BC,
+  				Double.parseDouble(freqABxBC.getText()));
+  		p.setSexualSelectionRate(Genotype.AB, Genotype.CC,
+  				Double.parseDouble(freqABxCC.getText()));
+
+      // BB x C_
+      p.setSexualSelectionRate(Genotype.BB, Genotype.AC,
+  				Double.parseDouble(freqBBxAC.getText()));
+  		p.setSexualSelectionRate(Genotype.BB, Genotype.BC,
+  				Double.parseDouble(freqBBxBC.getText()));
+  		p.setSexualSelectionRate(Genotype.BB, Genotype.CC,
+  				Double.parseDouble(freqBBxCC.getText()));
+
+      // AC x __
+      p.setSexualSelectionRate(Genotype.AC, Genotype.AA,
+  				Double.parseDouble(freqACxAA.getText()));
+  		p.setSexualSelectionRate(Genotype.AC, Genotype.AB,
+  				Double.parseDouble(freqACxAB.getText()));
+  		p.setSexualSelectionRate(Genotype.AC, Genotype.BB,
+  				Double.parseDouble(freqACxBB.getText()));
+      p.setSexualSelectionRate(Genotype.AC, Genotype.AC,
+  				Double.parseDouble(freqACxAC.getText()));
+  		p.setSexualSelectionRate(Genotype.AC, Genotype.BC,
+  				Double.parseDouble(freqACxBC.getText()));
+  		p.setSexualSelectionRate(Genotype.AC, Genotype.CC,
+  				Double.parseDouble(freqACxCC.getText()));
+
+      // BC x __
+      p.setSexualSelectionRate(Genotype.BC, Genotype.AA,
+  				Double.parseDouble(freqBCxAA.getText()));
+  		p.setSexualSelectionRate(Genotype.BC, Genotype.AB,
+  				Double.parseDouble(freqBCxAB.getText()));
+  		p.setSexualSelectionRate(Genotype.BC, Genotype.BB,
+  				Double.parseDouble(freqBCxBB.getText()));
+      p.setSexualSelectionRate(Genotype.BC, Genotype.AC,
+  				Double.parseDouble(freqBCxAC.getText()));
+  		p.setSexualSelectionRate(Genotype.BC, Genotype.BC,
+  				Double.parseDouble(freqBCxBC.getText()));
+  		p.setSexualSelectionRate(Genotype.BC, Genotype.CC,
+  				Double.parseDouble(freqBCxCC.getText()));
+
+      // CC x __
+      p.setSexualSelectionRate(Genotype.CC, Genotype.AA,
+  				Double.parseDouble(freqCCxAA.getText()));
+  		p.setSexualSelectionRate(Genotype.CC, Genotype.AB,
+  				Double.parseDouble(freqCCxAB.getText()));
+  		p.setSexualSelectionRate(Genotype.CC, Genotype.BB,
+  				Double.parseDouble(freqCCxBB.getText()));
+      p.setSexualSelectionRate(Genotype.CC, Genotype.AC,
+  				Double.parseDouble(freqCCxAC.getText()));
+  		p.setSexualSelectionRate(Genotype.CC, Genotype.BC,
+  				Double.parseDouble(freqCCxBC.getText()));
+  		p.setSexualSelectionRate(Genotype.CC, Genotype.CC,
+  				Double.parseDouble(freqCCxCC.getText()));
+    }
 	}
-	
+
 	public static void main(String[] args){
 		JFrame window = new JFrame();
-		
+
 		SexSelectPane test = new SexSelectPane();
-		
+
 		window.add(test);
 		window.pack();
 		window.setVisible(true);
